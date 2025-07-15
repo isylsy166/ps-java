29052599f97499f39e240c62f192c7637230394d
Soyeon <isylsy166@gmail.com>
2025-07-15 14:28:53 +0900

    leetcode:3136

---
+++ leetcode/src/No3136_Valid_Word.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class No3136_Valid_Word {
+    public static void main(String[] args){
+        String word = "a3$e";
+        char[] vowel =  {'a', 'e', 'i', 'o', 'u'};
+
+        if(word.length() < 3){
+            System.out.println(false);
+
+        }
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for(char c : word.toCharArray()){
+
+            // 영문자 or 숫자만 허용
+            if(!Character.isLetterOrDigit(c)) System.out.println(false);
+
+            if(Character.isLetter(c)){
+                char lower = Character.toLowerCase(c);
+                if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        System.out.println(hasVowel && hasConsonant ? true : false);
+
+    }
+}
